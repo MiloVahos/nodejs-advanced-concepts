3f80917210173fe81fa386fb48c5ab87adfb83c5
Camilo Vahos <milovahos@gmail.com>
2020-08-05 20:34:50 -0500

    Configure project

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb+srv://Admin:Camilo1996@tests-cluster.5xuwq.mongodb.net/tests?retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
