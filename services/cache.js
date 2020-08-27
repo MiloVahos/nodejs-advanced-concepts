082e18d72724aacba6b01a45bd0f533885544f2d
Camilo Vahos <milovahos@gmail.com>
2020-08-27 06:58:38 -0500

    Complete cache section

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,12 @@ const util =  require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get =  util.promisify(client.get);
+client.hget =  util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
   return this;
 }
 
@@ -21,7 +22,7 @@ mongoose.Query.prototype.exec = async function () {
     collection: this.mongooseCollection.name
     })
   );
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
   if ( cacheValue ) {
     const doc = JSON.parse(cacheValue);
     return Array.isArray(doc)
@@ -29,6 +30,12 @@ mongoose.Query.prototype.exec = async function () {
       : new this.model(doc);
   }
   const result = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(result), 'EX', 10);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
   return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 }
