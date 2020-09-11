0e72aa3e6f22e4d86121db76e6a698a0d2faae98
Camilo Vahos <milovahos@gmail.com>
2020-09-11 15:15:02 -0500

    Add Proxy logic to handle multiple classes

--- test/header.test.js
+++ test/header.test.js
@@ -1,14 +1,8 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
-
-let browser, page;
+const Page = require('./helpers/page');
+let page;
 
 beforeEach( async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
@@ -24,16 +18,11 @@ test('Clicking login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-  await page.setCookie({ name: 'session', value: session });
-  await page.setCookie({ name: 'session.sig', value: sig });
-  await page.goto('localhost:3000');
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
   expect(text).toEqual('Logout');
 });
 
 afterEach( async () => {
-  await browser.close();
+  await page.close();
 });
